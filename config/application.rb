52b4b35f39c96f53fa16c41a8a3a37c399e11dc6
Miles Zimmerman <mzimmerman@invoca.com>
2015-11-16 15:59:09 -0800

    Adding sorcery files

--- config/application.rb
+++ config/application.rb
@@ -8,19 +8,34 @@ Bundler.require(*Rails.groups)
 
 module Squid
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Set the timezone
+    config.time_zone = 'Pacific Time (US & Canada)'
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    # Load Grape API
+    config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # Disable assets
+    config.assets.enabled = false
+
+    # Set ORM and disable asset generation
+    config.generators do |g|
+      g.assets false
+      g.orm :active_record
+    end
+
+    # Route exceptions back to Grape API
+    config.exceptions_app = self.routes
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Cors
+    # config.middleware.insert_before 0, "Rack::Cors" do
+    #   allow do
+    #     origins ENV['CORS_ORIGINS']
+    #     resource '*', :headers => :any, :methods => [:get, :put, :post, :patch, :options]
+    #   end
+    # end
   end
 end
